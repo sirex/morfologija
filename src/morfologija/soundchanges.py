948484900ca19ea6def75365140cf1e25f7f5ca6
sirex <sirexas@gmail.com>
2014-04-02 15:45:38 +0300

    Moved syllabification to place where it should be.

--- src/morfologija/soundchanges.py
+++ src/morfologija/soundchanges.py
@@ -1,12 +1,12 @@
 from .utils import first
-from .sounds import itervowels
+from .sounds import iter_vowels
 from .sounds import BACK_VOWELS
 
 
 def affrication(stem, suffix):
     stem_ending = stem[-1]
     changes = {'d': 'dž', 't': 'č'}
-    vowels = (first(itervowels(suffix)) or '')[:2]
+    vowels = (first(iter_vowels(suffix)) or '')[:2]
     triggers = ['i' + s for s in BACK_VOWELS]
     if stem_ending in ('d', 't') and vowels in triggers:
         return stem[:-len(stem_ending)] + changes[stem_ending]
--- src/morfologija/sounds.py
+++ src/morfologija/sounds.py
@@ -3,7 +3,7 @@ FRON_VOWELS = 'iįyeęė'
 BACK_VOWELS = 'aąouųū'
 
 
-def itervowels(chars):
+def iter_vowels(chars):
     vowels = []
     for c in chars:
         vowel = c in VOWELS
@@ -14,3 +14,17 @@ def itervowels(chars):
             vowels = []
     if vowels:
         yield ''.join(vowels)
+
+
+def split_sounds(word):
+    last = ''
+    for char in word:
+        if char in {'z', 'ž'} and last == 'd':
+            yield last + char
+            last = ''
+            continue
+        if last:
+            yield last
+        last = char
+    if last:
+        yield last
